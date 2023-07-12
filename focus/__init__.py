af20b951784e9d1afef3ea40b1566145e1b665e0
Kyosuke Morita <kyosuke1029@icloud.com>
2023-07-12 16:08:03 +0100

    fix data types

---
+++ focus/__init__.py
@@ -0,0 +1,19 @@
+from .core import Focus
+
+# Let users know if they're missing any of our hard dependencies
+_hard_dependencies = ("tensorflow", "numpy")
+_missing_dependencies = []
+
+for _dependency in _hard_dependencies:
+    try:
+        __import__(_dependency)
+    except ImportError as _e:  # pragma: no cover
+        _missing_dependencies.append(f"{_dependency}: {_e}")
+
+if _missing_dependencies:  # pragma: no cover
+    raise ImportError(
+        "Unable to import required dependencies:\n" + "\n".join(_missing_dependencies)
+    )
+del _hard_dependencies, _dependency, _missing_dependencies
+
+__all__ = ["Focus"]
